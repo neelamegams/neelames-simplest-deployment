3c13f79018f75ad62322708c58f15a6455b30134
Shrinivasan Neelamegam <shrinivasan.neelamegam@sap.com>
2024-07-11 22:24:35 +0530

    task3-basic-sum-dev-chal-jul2024

--- srv/basic.cds
+++ srv/basic.cds
@@ -2,4 +2,5 @@
 service basic{
     function ping() returns String;
     function hello (to:String) returns String;
+    function sum (a:Integer, b:Integer) returns Integer;
 }
--- srv/basic.js
+++ srv/basic.js
@@ -1,4 +1,5 @@
 module.exports = function (){
     this.on('ping', () => "pong")
     this.on('hello', (req) => `Hello ${req.data.to}!`)
+    this.on('sum', ({data:{a,b}}) => a+b)
 }
